b51b0937684e7acf4784fa0ddce01c8ac9e2bc7f
HughOwenPanopio <panopiohughowen@gmail.com>
2024-05-17 13:41:54 +0800

    added comments to explain the block of codes in the useKeyState

--- src/CustomHooks/useKeyState.jsx
+++ src/CustomHooks/useKeyState.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 
+//*! This is a custom hook, a state that can be reused accross multiple instances of the same component. it accepts the props of key(keycode on keyboard) and action(the callback function). Line 5 is the useEffect hook that is used to handle a side effect, it contains the the logical function and a dependency array at like 18 that will render every update of the DOM element. Line 15 is the cleanup function that is used to cleanup the DOM element after it has been removed or unmount.
 export function useKeyState(key, action) {
   useEffect(
     function () {
